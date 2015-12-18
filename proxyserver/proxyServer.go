a1751f1ef107d76e986556514a7e11374e3b014f
Rishi <rishiloyola98245@gmail.com>
2015-12-18 18:34:18 +0530

    Integrate with zookeeper

--- proxyserver/proxyServer.go
+++ proxyserver/proxyServer.go
@@ -7,21 +7,24 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/sacheendra/api_frontend/config"
 	"github.com/sacheendra/api_frontend/infrastructure"
+	"github.com/samuel/go-zookeeper/zk"
+	"stathat.com/c/consistent"
 )
 
 var PushpinServerAddr string
-var HTTPServerAddr string
+
+//var HTTPServerAddr string
 var client *http.Client
+var zookeeperConn *zk.Conn
+var hash *consistent.Consistent
 
 func main() {
 	serverInstance := Init()
-	fmt.Println("running server...")
 	fmt.Println("listening on " + serverInstance.Addr)
 	log.Fatal(serverInstance.ListenAndServe())
 }
@@ -30,10 +33,11 @@ func Init() *http.Server {
 
 	currentConfig := getCurrentConfig()
 	PushpinServerAddr = currentConfig.PushpinServers.ProxyAddress
-	HTTPServerAddr = strconv.Itoa(currentConfig.HTTPServer.Port)
+	//HTTPServerAddr = strconv.Itoa(currentConfig.HTTPServer.Port)
 	client = &http.Client{}
-	fmt.Println("Initializing server..." + PushpinServerAddr)
 	fmt.Println("Environment: " + currentConfig.Name)
+	zkconnectClient()
+	getWatch()
 	return &http.Server{
 		Addr:           ":7000",
 		Handler:        http.HandlerFunc(routeReq),
@@ -41,6 +45,37 @@ func Init() *http.Server {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+}
+
+func getWatch() {
+	snapshots, errors := setWatch(zookeeperConn, "/HTTPserver")
+	fmt.Println("[zookeeper] : Set watch on path /HTTPserver")
+	go func() {
+		for {
+			select {
+			case snapshot := <-snapshots:
+				hash = consistent.New()
+				for i := 0; i < len(snapshot); i++ {
+					res, _, _ := zookeeperConn.Get("/HTTPserver/" + snapshot[i])
+					hash.Add(string(res[:]))
+				}
+			case err := <-errors:
+				panic(err)
+			}
+		}
+	}()
+
+}
+
+func zkconnectClient() {
+	var err error
+	zookeeperConn, _, err = zk.Connect([]string{"127.0.0.1:2181"}, time.Second)
+	handleError(err)
+	time.Sleep(time.Second)
+	// path, err := zookeeperConn.Create("/localserver", []byte("data"), int32(zk.FlagEphemeral), zk.WorldACL(zk.PermAll))
+	// handleError(err)
+	// time.Sleep(time.Second)
+	fmt.Println("[zookeeper] : Connected with Zookeeper...")
 }
 
 func getCurrentConfig() config.EnvironmentConfig {
@@ -74,14 +109,18 @@ func routeReq(w http.ResponseWriter, req *http.Request) {
 		path = "http://" + user + ":" + pass + "@" + PushpinServerAddr + req.URL.String()
 		fmt.Println("started " + req.Method + " " + req.URL.String())
 	} else {
-		path = "http://" + user + ":" + pass + "@localhost:" + HTTPServerAddr + req.URL.String()
+		server, err := hash.Get(user)
+		if err != nil {
+			log.Fatal(err)
+		}
+		path = "http://" + user + ":" + pass + "@" + server + req.URL.String()
 		fmt.Println("Started " + req.Method + " " + req.URL.String())
 	}
 
 	remoteReq, err := http.NewRequest(req.Method, path, req.Body)
 
 	if err != nil {
-		log.Println("error : ", err)
+		fmt.Println(err)
 	}
 
 	remoteResp, err := client.Do(remoteReq)
@@ -118,6 +157,28 @@ func identifyReqType(path *url.URL) bool {
 //handleError stops the program execution
 func handleError(err error) {
 	if err != nil {
+		zookeeperConn.Close()
 		panic(err)
 	}
+}
+
+func setWatch(conn *zk.Conn, path string) (chan []string, chan error) {
+	snapshots := make(chan []string)
+	errors := make(chan error)
+	go func() {
+		for {
+			snapshot, _, events, err := conn.ChildrenW(path)
+			if err != nil {
+				errors <- err
+				return
+			}
+			snapshots <- snapshot
+			evt := <-events
+			if evt.Err != nil {
+				errors <- evt.Err
+				return
+			}
+		}
+	}()
+	return snapshots, errors
 }
